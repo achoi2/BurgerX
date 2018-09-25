d4ac4a1795ad36ed768889b902841db930b08b41
Nat Sharpe <sharpe1890@gmail.com>
2018-09-25 14:16:04 -0400

    Populates MenuPage with items from store

---
+++ client/src/Components/MenuPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+const MenuPage = (props) => {
+  return (
+    <div>
+      <h1>Menu</h1>
+      <ul>
+        {
+          props.menu.map(item =>
+            <li key={item.id}>
+              <h2>{item.title}</h2>
+              <p>{item.details}</p>
+              <h3>{`$${item.price}`}</h3>
+              <p>{`${item.thumbsup}👍`}</p>
+            </li> 
+          )
+        }
+      </ul>
+    </div>
+  )
+};
+
+export default connect( (state) => ({menu : state.menu}) )(MenuPage);
