5bf80ee38a2ab257bf884b0eecd9160fc54c0cfe
Andrew <andrewchoi1@hotmail.com>
2018-09-26 15:32:53 -0400

    fixed navbar

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -5,16 +5,12 @@ const NavBar = () => {
   return (
     <nav>
       <ul className="nav">
-        <li>
-          <Link to="/" className="nav-link active">Home</Link>
-        </li>
-        <li>
-          <Link to="/reservations" className="nav-link active">Reservations</Link>
-        </li>
-        <li><Link to="/menu">Menu</Link></li>
-        <li><Link to="/checkout">Checkout</Link></li>
-        <li><Link to="/reserved">Reserved</Link></li>
-        <li><Link to="/admin">Log In</Link></li>
+        <li><Link to="/" className="nav-link active">Home</Link></li>
+        <li><Link to="/reservations" className="nav-link active">Reservations</Link></li>
+        <li><Link to="/menu" className="nav-link active">Menu</Link></li>
+        <li><Link to="/checkout" className="nav-link active">Checkout</Link></li>
+        <li><Link to="/reserved" className="nav-link active">Reserved</Link></li>
+        <li><Link to="/admin" className="nav-link active">Log In</Link></li>
       </ul>
     </nav>
   )
