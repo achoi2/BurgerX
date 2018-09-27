24aa5c3e2c338955320ba12c69a2d9cb489295d6
Nat Sharpe <sharpe1890@gmail.com>
2018-09-27 13:59:37 -0400

    Adds newitem route to backend menu

--- routes/api/menu.js
+++ routes/api/menu.js
@@ -11,4 +11,32 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/newitem", (req, res) => {
+  let newMenuItem = {
+    title: req.body.title,
+    imageurl: req.body.imageurl,
+    details: req.body.details,
+    price: req.body.price,
+    thumbsup: req.body.thumbsup
+  };
+  DB.one(
+    `INSERT INTO menu (title, imageurl, details, price, thumbsup)
+  VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
+    [
+      newMenuItem.title,
+      newMenuItem.imageurl,
+      newMenuItem.details,
+      newMenuItem.price,
+      newMenuItem.thumbsup
+    ]
+  )
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.send({ error: err });
+    });
+});
+
 module.exports = router;
