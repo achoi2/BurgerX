9702ae8b71aa0961c08da2e94ca8bafe814787b8
Matheus Duarte <mfduar8766@gmail.com>
2018-09-27 15:25:11 -0400

    Empty menu state.

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -2,36 +2,7 @@ import { createStore } from "redux";
 import reducer from "./reducers.js";
 
 let initialState = {
-  menu: [
-    {
-      id: "1",
-      title: "Boring Burger",
-      details: "Charred to perfection by our homemade flamethrower",
-      price: 10,
-      thumbsup: 2
-    },
-    {
-      id: "2",
-      title: "Tesla Curly Fries",
-      details: "Innovative coils of greasy goodness",
-      price: 3,
-      thumbsup: 5
-    },
-    {
-      id: "3",
-      title: "Space Shake",
-      details: "Cool your jets with this steller milkshake",
-      price: 6,
-      thumbsup: 9
-    },
-    {
-      id: "4",
-      title: "Mars Nuggets",
-      details: "Best fried chicken on the planet",
-      price: 8,
-      thumbsup: 3
-    }
-  ],
+  menu: [],
   cartItems: [],
   backlog:[],
   reservations:[],
